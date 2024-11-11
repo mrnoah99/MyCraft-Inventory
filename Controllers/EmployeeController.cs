afb3ddca13142585a6850bd1183ed2ded3fb5faf
Noah <nbaldwin3@my.bcit.ca>
2024-11-11 05:36:56 -0800

    Fixed local repo pushing /bin and /obj, created Employee pages.

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -17,10 +17,29 @@ namespace MyCraft_Inventory.Controllers
         // [Authorize]
         public IActionResult Inventory()
         {
-            InventoryObjectModel inventory = new InventoryObjectModel {ItemName = "bob", ItemDescription = "bob", ItemPrice = 50.99, Qty = 5, InStock=false};
-            InventoryObjectModel inventory2 = new InventoryObjectModel {ItemName = "john", ItemDescription = "john", ItemPrice = 69.99, Qty = 72, InStock=false};
+            InventoryObjectModel inventory = new InventoryObjectModel {ItemName = "bob", ItemDescription = "bob", ItemPrice = 50.99, Qty = 5, InStock = false};
+            InventoryObjectModel inventory2 = new InventoryObjectModel {ItemName = "john", ItemDescription = "john", ItemPrice = 69.99, Qty = 72, InStock = false};
             InventoryObjectModel[] test = {inventory, inventory2};
             ViewBag.items = test;
+            return View();
+        }
+
+        public IActionResult Supplies() {
+            InventoryObjectModel supply1 = new InventoryObjectModel {ItemName = "bob", ItemDescription = "bob", ItemPrice = 50.99, Qty = 5, InStock = true};
+            InventoryObjectModel supply2 = new InventoryObjectModel {ItemName = "john", ItemDescription = "john", ItemPrice = 69.99, Qty = 72, InStock = true};
+            InventoryObjectModel supply3 = new InventoryObjectModel {ItemName = "jeb", ItemDescription = "jeb", ItemPrice = 120.99, Qty = 1, InStock = true};
+            InventoryObjectModel supply4 = new InventoryObjectModel {ItemName = "oops", ItemDescription = "out of stock item", ItemPrice = 1.01, Qty = 0, InStock = false};
+            InventoryObjectModel[] test = {supply1, supply2, supply3, supply4};
+            ViewBag.availableItems = test;
+            return View();
+        }
+
+        public IActionResult TransactionHistory() {
+            TransactionObjectModel transaction1 = new TransactionObjectModel {Date = DateTime.Now, Amount = 40.99, ID = 1, IsSale = false};
+            TransactionObjectModel transaction2 = new TransactionObjectModel {Date = DateTime.Now, Amount = 50.99, ID = 2, IsSale = false};
+            TransactionObjectModel transaction3 = new TransactionObjectModel {Date = new DateTime(12345678910), Amount = 78.99, ID = 3, IsSale = true};
+            TransactionObjectModel[] test = {transaction1, transaction2, transaction3};
+            ViewBag.transactions = test;
             return View();
         }
     }
